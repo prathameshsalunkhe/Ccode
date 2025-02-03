aa683e27f11ce9959d718c9791f5b7d8ddc248ed
Prathamesh Salunkhe <86035718+prathameshsalunkhe@users.noreply.github.com>
2025-02-03 11:12:13 +0530

    Update bitmask_deldig.c
    correction

--- bitmask_deldig.c
+++ bitmask_deldig.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 void main()
 {
-	unsigned int num=93,r1,r2;
+	unsigned int num=89,r1,r2;
 	printf("Befor Swaping=%x %d \n",num,num);
-		//num=num|0x00000000;
+		
 		r1=num>>3;
-		r1=num<<1;
+		r1=num<<3;
 		r2=num<<7;
 		r2=r2>>7;
 		num=r1|r2;
