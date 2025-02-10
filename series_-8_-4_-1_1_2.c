3efaebae44c7939aceef1e4f827310661916df4a
Prathamesh Salunkhe <86035718+prathameshsalunkhe@users.noreply.github.com>
2025-02-10 17:26:52 +0530

    Add files via upload

---
+++ series_-8_-4_-1_1_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int i=6,j;
+	for (j=-19;i>0;i--)
+	{	j=j+i;
+		printf(" %d ",j);
+	}
+		printf(" \n ");
+}
---
+++ series_1_9_28.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	for (i=1;i<50;i++)
+	{	j=i*i*i+1;
+		printf(" %d ",j);
+	}
+}
